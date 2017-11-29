f1c5052071a87df75b0567ee1f60d47a6ce11f99
Ismayil Hasanov <ismailhasanov@gmail.com>
2017-11-29 11:53:08 -0500

    Added a pin for each favorite on the map.

--- WhatsThat/view controllers/FavoritePhotosMapViewController.swift
+++ WhatsThat/view controllers/FavoritePhotosMapViewController.swift
@@ -2,7 +2,7 @@
 //  FavoritePhotosMapViewController.swift
 //  WhatsThat
 //
-//  Created by Ismayil Hasanov on 11/28/17.
+//  Created by Ismayil Hasanov on 11/29/17.
 //  Copyright © 2017 GWU. All rights reserved.
 //
 
@@ -10,8 +10,20 @@ import UIKit
 import MapKit
 
 class FavoritePhotosMapViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var favorites = [FavoriteIdentification]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Fetch the favorites
+        favorites = Persistance.sharedInstance.fetchIdentifications()
+        favorites.forEach { favorite in
+            mapView.addAnnotation(favorite)
+        }
     }
 }
